73aa94a261fc3c6906a850ad947461d04fd5e2b2
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2013-06-29 10:03:50 -0400

    Starting an example of working with files, and splitting them up

---
+++ hex-example/Encode.hs
@@ -0,0 +1,32 @@
+#!/usr/bin/env runhaskell
+
+import qualified Data.Text.Lazy.IO as LIO
+import Data.Text.Lazy (Text, transpose, chunksOf)
+
+numBanks = 8
+basename = "bank_"
+suffix = ".hex"
+
+
+-- chunksOf splits a Text into components of length k
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = Prelude.zip [0..]
+
+-- | Take every character in Text, and encode it into 
+-- | its hex code. One per line
+{-toHexDump :: Text -> Text-}
+
+-- | Given a basename, and a list of texts,
+-- | write each one out to <basename><N>.<suffix>
+dumpToBankFiles :: FilePath -> String -> [Text] -> IO ()
+dumpToBankFiles basename suffix texts = do
+    let indexesAndTexts = enumerate texts
+    mapM_ (\(i, text) -> LIO.writeFile (makeFilepath i) text) indexesAndTexts
+    where
+        makeFilepath i = basename ++ show i ++ suffix
+
+main = do
+    lazyText <- LIO.readFile "input-text.txt"
+    dumpToBankFiles basename suffix $ transpose $ chunksOf numBanks lazyText
+
